64fc2670ebfe3f69b3241f91ab1be354241f17ab
heyeway <heyewayxysxd@gmail.com>
2020-07-10 04:07:18 -0400

    LPTHW exercise 23,24,25,26

---
+++ ex23.py
@@ -0,0 +1,22 @@
+import sys
+script, input_encoding, error = sys.argv
+
+def main(language_file, encoding, errors):
+    line = language_file.readline()
+
+    if line:
+        print_line(line, encoding, errors)
+        return main(language_file, encoding, errors)
+
+def print_line(line, encoding, errors):
+    next_lang = line.strip()
+    # type(next_lang) is str
+    raw_bytes = next_lang.encode(encoding, errors=errors)
+    # type(raw_bytes) is bytes
+    cooked_string = raw_bytes.decode(encoding, errors=errors)
+
+    print(raw_bytes, "<===>", cooked_string)
+
+languages = open("languages.txt", encoding="utf-8")
+
+main(languages, input_encoding, error)
